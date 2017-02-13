682dcf00dd537cde61e2b9c716cab1c8f7f8b413
croutons <myxie>
2017-02-13 22:02:34 +0800

    Fixed nilClass errors; cleaning up CSS/html

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -6,8 +6,8 @@ class SignupsController < ApplicationController
 
   def create
     @signup = Signup.new(params.require(:signup).permit(:email))
-      if @visitor.save
-      redirect_to root_path, notice: "Signed up #{@signup.email}."
+      if @signup.save
+      	redirect_to root_path, notice: "Signed up #{@signup.email}."
     else
       render :new
     end
